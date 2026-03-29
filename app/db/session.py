1911707311cf96e68b2b6adeecbae77cb0ddea49
Alain Alvarez <alalvarez@soaint.com>
2026-03-29 10:18:05 -0400

    sdsd

--- app/db/session.py
+++ app/db/session.py
@@ -5,8 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,
+                       pool_size=3,
+                       max_overflow=2,
+                       pool_timeout=30,
+                       pool_recycle=900,
+                       pool_pre_ping=True,
+                       pool_use_lifo=True,
+                       )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db = SessionLocal()
