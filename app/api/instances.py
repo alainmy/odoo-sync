0bfe8767fc44b72ecadca827e2e0de5b348bcf78
Alain Alvarez <alalvarez@soaint.com>
2026-04-11 18:06:01 -0400

    Cambios

--- app/api/instances.py
+++ app/api/instances.py
@@ -70,39 +70,45 @@ def create_instance(
     db: Session = Depends(get_db),
     current_user: Admin = Depends(get_current_user)
 ):
-    # Obtener propiedasdes del odoo como modulos instalados, version, etc para guardarlas en la instancia
-    odoo_client = OdooClient(
-        url=instance.odoo_url,
-        db=instance.odoo_db,
-        username=instance.odoo_username,
-        password=instance.odoo_password
-    )
-    uid = odoo_client.odoo_authenticate()
-    if not uid:
+    try:
+        # Obtener propiedasdes del odoo como modulos instalados, version, etc para guardarlas en la instancia
+        odoo_client = OdooClient(
+            url=instance.odoo_url,
+            db=instance.odoo_db,
+            username=instance.odoo_username,
+            password=instance.odoo_password
+        )
+        uid = odoo_client.odoo_authenticate()
+        if not uid:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No se pudo conectar a Odoo con las credenciales proporcionadas"
+            )
+        odoo_info = odoo_client.get_odoo_info()
+        version_info = odoo_info.get("server_version", "Unknown")
+        # Get instaled modules
+        installed_modules = odoo_client.get_installed_modules()
+        modules = [module.get("name") for module in installed_modules if module["name"] in [
+            "sale_management", "stock", "account", "product", "website_sale"]]
+        installed_modules_names = os.linesep.join(
+            [module for module in modules])
+        odoo_description = f"Odoo Version {version_info}\n with modules:\n \n{installed_modules_names}"
+        instance.odoo_description = odoo_description
+        instance_created = crud_instance.create_instance(
+            db, instance=instance, user_id=current_user.id)
+        # Get or create webhook for update order in WooCommerce
+        if 'sale_management' in modules:
+            odoo_webhook_url_template = "{host}/api/v1/webhook-receiver/odoo/{instance_id}/order_update"
+            webhook = odoo_client.get_webhook_by_url(
+                url=odoo_webhook_url_template.format(host=settings.fast_api_host,
+                                                    instance_id=instance_created.id))
+        """Crear una nueva instancia"""
+        return instance_created
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="No se pudo conectar a Odoo con las credenciales proporcionadas"
+            detail=f"Error: {str(e)}"
         )
-    odoo_info = odoo_client.get_odoo_info()
-    version_info = odoo_info.get("server_version", "Unknown")
-    # Get instaled modules
-    installed_modules = odoo_client.get_installed_modules()
-    modules = [module.get("name") for module in installed_modules if module["name"] in [
-        "sale_management", "stock", "account", "product", "website_sale"]]
-    installed_modules_names = os.linesep.join(
-        [module for module in modules])
-    odoo_description = f"Odoo Version {version_info}\n with modules:\n \n{installed_modules_names}"
-    instance.odoo_description = odoo_description
-    instance_created = crud_instance.create_instance(
-        db, instance=instance, user_id=current_user.id)
-    # Get or create webhook for update order in WooCommerce
-    if 'sale_management' in modules:
-        odoo_webhook_url_template = "{host}/api/v1/webhook-receiver/odoo/{instance_id}/order_update"
-        webhook = odoo_client.get_webhook_by_url(
-            url=odoo_webhook_url_template.format(host=settings.fast_api_host,
-                                                 instance_id=instance_created.id))
-    """Crear una nueva instancia"""
-    return instance_created
 
 
 @router.put("/{instance_id}", response_model=WooCommerceInstance)
@@ -113,47 +119,53 @@ def update_instance(
     current_user: Admin = Depends(get_current_user)
 ):
     # Obtener propiedasdes del odoo como modulos instalados, version, etc para guardarlas en la instancia
-    odoo_client = OdooClient(
-        url=instance_update.odoo_url,
-        db=instance_update.odoo_db,
-        username=instance_update.odoo_username,
-        password=instance_update.odoo_password
-    )
-    uid = odoo_client.odoo_authenticate()
-    if not uid:
+    try:
+        odoo_client = OdooClient(
+            url=instance_update.odoo_url,
+            db=instance_update.odoo_db,
+            username=instance_update.odoo_username,
+            password=instance_update.odoo_password
+        )
+        uid = odoo_client.odoo_authenticate()
+        if not uid:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No se pudo conectar a Odoo con las credenciales proporcionadas"
+            )
+        odoo_info = odoo_client.get_odoo_info()
+        version_info = odoo_info.get("server_version", "Unknown")
+        # Get instaled modules
+        installed_modules = odoo_client.get_installed_modules()
+        modules = [module.get("name") for module in installed_modules if module["name"] in [
+            "sale_management", "stock", "account", "product", "website_sale"]]
+        installed_modules_names = os.linesep.join(
+            [module for module in modules])
+        odoo_description = f"Odoo Version {version_info}\n with modules:\n \n{installed_modules_names}"
+        instance_update.odoo_description = odoo_description
+        """Actualizar una instancia"""
+
+        if 'sale_management' in modules:
+            odoo_webhook_url_template = "{host}/api/v1/webhook-receiver/odoo/{instance_id}/order_update"
+            webhook = odoo_client.get_webhook_by_url(
+                url=odoo_webhook_url_template.format(host=settings.fast_api_host,
+                                                    instance_id=instance_id))
+        instance = crud_instance.update_instance(
+            db,
+            instance_id=instance_id,
+            user_id=current_user.id,
+            instance_update=instance_update
+        )
+        if not instance:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Instancia no encontrada"
+            )
+        return instance
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="No se pudo conectar a Odoo con las credenciales proporcionadas"
+            detail=f"Error: {str(e)}"
         )
-    odoo_info = odoo_client.get_odoo_info()
-    version_info = odoo_info.get("server_version", "Unknown")
-    # Get instaled modules
-    installed_modules = odoo_client.get_installed_modules()
-    modules = [module.get("name") for module in installed_modules if module["name"] in [
-        "sale_management", "stock", "account", "product", "website_sale"]]
-    installed_modules_names = os.linesep.join(
-        [module for module in modules])
-    odoo_description = f"Odoo Version {version_info}\n with modules:\n \n{installed_modules_names}"
-    instance_update.odoo_description = odoo_description
-    """Actualizar una instancia"""
-
-    if 'sale_management' in modules:
-        odoo_webhook_url_template = "{host}/api/v1/webhook-receiver/odoo/{instance_id}/order_update"
-        webhook = odoo_client.get_webhook_by_url(
-            url=odoo_webhook_url_template.format(host=settings.fast_api_host,
-                                                 instance_id=instance_id))
-    instance = crud_instance.update_instance(
-        db,
-        instance_id=instance_id,
-        user_id=current_user.id,
-        instance_update=instance_update
-    )
-    if not instance:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Instancia no encontrada"
-        )
-    return instance
 
 
 @router.delete("/{instance_id}", status_code=status.HTTP_204_NO_CONTENT)
