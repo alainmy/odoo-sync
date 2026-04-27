e4faa6eeac219ae15a2f4f424a3dbca2cbe050bd
Alain Alvarez <alalvarez@soaint.com>
2026-04-27 10:50:07 -0400

    cambios

--- app/main.py
+++ app/main.py
@@ -25,10 +25,15 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from app.session import get_session, create_session, lifespan
 _logger = logging.getLogger(__name__)
 
-  # Cliente global
+# Cliente global
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan,
+              root_path="/api",
+              docs_url="/docs",
+              redoc_url="/redoc",
+              openapi_url="/openapi.json"
+              )
 app.mount(
     "/images",
     StaticFiles(directory="app/images"),
@@ -73,13 +78,13 @@ class MySessionMiddleware(BaseHTTPMiddleware):
             #         "lang": lang,
             #         "tz": tz,
             #         "website_id": int(website_id),
-            # }         
+            # }
             # if not session:
             #     # if not session.is_valid() and session.expiry_date:
             #     #     raise HTTPException(status_code=401, detail="Session expired")
             #     new_session_data = {}
             #     new_session_data["context"] = context
-            #     create_session(session_id, new_session_data)     
+            #     create_session(session_id, new_session_data)
             # else:
             #     session_data = json.loads(session.data)
             #     session_data["context"].update(context)
@@ -99,22 +104,32 @@ class MySessionMiddleware(BaseHTTPMiddleware):
 app.include_router(admin_router, prefix="/admin", tags=["admin"])
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(odoo_router, prefix="/odoo", tags=["odoo"])
-app.include_router(woocommerce_router, prefix="/woocommerce", tags=["woocommerce"])
+app.include_router(woocommerce_router,
+                   prefix="/woocommerce", tags=["woocommerce"])
 # app.include_router(books_router, prefix="/bookscraping", tags=["bookscraping"])
 app.include_router(invoice_router, prefix="/invoice", tags=["invoice"])
 app.include_router(projects_router, prefix="/projects", tags=["projects"])
 app.include_router(sync_logs_router, prefix="/api/v1/sync", tags=["sync-logs"])
-app.include_router(task_monitoring_router, prefix="/api/v1/sync", tags=["task-monitoring"])
-app.include_router(sync_management_router, prefix="/api/v1", tags=["sync-management"])
-app.include_router(category_tag_management_router, prefix="/api/v1", tags=["category-tag-management"])
+app.include_router(task_monitoring_router,
+                   prefix="/api/v1/sync", tags=["task-monitoring"])
+app.include_router(sync_management_router, prefix="/api/v1",
+                   tags=["sync-management"])
+app.include_router(category_tag_management_router,
+                   prefix="/api/v1", tags=["category-tag-management"])
 app.include_router(attributes_router, prefix="/api/v1", tags=["attributes"])
-app.include_router(attributes_management_router, prefix="/api/v1", tags=["attribute-management"])
+app.include_router(attributes_management_router,
+                   prefix="/api/v1", tags=["attribute-management"])
 app.include_router(instances_router, prefix="/api/v1", tags=["instances"])
-app.include_router(pricelists_router, prefix="/api/v1/pricelists", tags=["pricelists"])
-app.include_router(webhooks_router, prefix="/api/v1/webhooks", tags=["webhooks"])
-app.include_router(webhook_receiver_router, prefix="/api/v1/webhook-receiver", tags=["webhook-receiver"])
-app.include_router(delivery_methods, prefix="/api/v1/delivery_methods", tags=["DELIVERY"])
-app.include_router(odoo_websites_router, prefix="/api/v1/odoo-websites", tags=["odoo-websites"])
+app.include_router(pricelists_router,
+                   prefix="/api/v1/pricelists", tags=["pricelists"])
+app.include_router(
+    webhooks_router, prefix="/api/v1/webhooks", tags=["webhooks"])
+app.include_router(webhook_receiver_router,
+                   prefix="/api/v1/webhook-receiver", tags=["webhook-receiver"])
+app.include_router(
+    delivery_methods, prefix="/api/v1/delivery_methods", tags=["DELIVERY"])
+app.include_router(odoo_websites_router,
+                   prefix="/api/v1/odoo-websites", tags=["odoo-websites"])
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=5010, reload=True)
