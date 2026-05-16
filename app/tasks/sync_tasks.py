5da25b406d4b459f43e6350ab3c95bf333c8f1d8
Alain Alvarez <alalvarez@soaint.com>
2026-05-16 15:31:30 -0400

    cambios

--- app/tasks/sync_tasks.py
+++ app/tasks/sync_tasks.py
@@ -522,7 +522,7 @@ def sync_product_to_odoo(self,
             logger.info(
                 f"Found sync record for WooCommerce product ID {woo_id}, Odoo ID {prododuct_sync.odoo_id}")
 
-            existing_products = client.search_read(
+            existing_products = client.search_read_sync(
                 uid=uuid,
                 model="product.product",
                 domain=[("id", "=", prododuct_sync.odoo_id)],
